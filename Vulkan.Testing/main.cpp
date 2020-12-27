f513aefbf23a71d44c40fec0742192cb992d898f
Callum Anning <CallumAnning@hotmail.co.uk>
2020-12-27 20:01:23 +0000

    Some Vulkan

--- Vulkan.Testing/main.cpp
+++ Vulkan.Testing/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include <SDL.h>
+#include <SDL_vulkan.h>
 #include "Timer.h"
+
+#include <vulkan/vulkan.hpp>
+
 
 namespace
 {
@@ -47,6 +52,69 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	// Vulkan
+	unsigned extension_count;
+	if (!SDL_Vulkan_GetInstanceExtensions(window, &extension_count, NULL)) 
+	{
+		std::cout << "Could not get the number of required instance extensions from SDL." << std::endl;
+		return 1;
+	}
+
+	std::vector<const char*> extensions(extension_count);
+	if (!SDL_Vulkan_GetInstanceExtensions(window, &extension_count, extensions.data())) 
+	{
+		std::cout << "Could not get the names of required instance extensions from SDL." << std::endl;
+		return 1;
+	}
+
+	// Use validation layers if this is a debug build
+	std::vector<const char*> layers;
+#if defined(_DEBUG)
+	layers.push_back("VK_LAYER_KHRONOS_validation");
+#endif
+
+	// vk::ApplicationInfo allows the programmer to specifiy some basic information about the
+	// program, which can be useful for layers and tools to provide more debug information.
+	vk::ApplicationInfo appInfo = vk::ApplicationInfo()
+		.setPApplicationName("Vulkan C++ Windowed Program Template")
+		.setApplicationVersion(1)
+		.setPEngineName("LunarG SDK")
+		.setEngineVersion(1)
+		.setApiVersion(VK_API_VERSION_1_0);
+	// vk::InstanceCreateInfo is where the programmer specifies the layers and/or extensions that
+	// are needed.
+
+	vk::InstanceCreateInfo instInfo = vk::InstanceCreateInfo()
+		.setFlags(vk::InstanceCreateFlags())
+		.setPApplicationInfo(&appInfo)
+		.setEnabledExtensionCount(static_cast<uint32_t>(extensions.size()))
+		.setPpEnabledExtensionNames(extensions.data())
+		.setEnabledLayerCount(static_cast<uint32_t>(layers.size()))
+		.setPpEnabledLayerNames(layers.data());
+
+	// Create the Vulkan instance.
+	vk::Instance instance;
+	try {
+		instance = vk::createInstance(instInfo);
+	}
+	catch (const std::exception& e) {
+		std::cout << "Could not create a Vulkan instance: " << e.what() << std::endl;
+		return 1;
+	}
+
+	// Create a Vulkan surface for rendering
+	VkSurfaceKHR c_surface;
+	if (!SDL_Vulkan_CreateSurface(window, static_cast<VkInstance>(instance), &c_surface)) {
+		std::cout << "Could not create a Vulkan surface." << std::endl;
+		return 1;
+	}
+
+	vk::SurfaceKHR surface(c_surface);
+
+
+
+
+
 	// Timer
 	Timer timer;
 	timer.Start();
@@ -63,10 +131,16 @@ int main(int argc, char** argv)
 		else
 		{
 			CalculateFPS(&timer, window);
+
+
 		}
 	}
 
+	// Clean up
+	instance.destroySurfaceKHR(surface);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+	instance.destroy();
+
 	return 0;
 }
